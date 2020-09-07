3539bdd8518ede0902c48969efedf6cc11c8415d
urbanz91 <turbino4ka91@gmail.com>
2020-09-07 13:50:53 +0300

    initial scene

---
+++ Assets/Scripts/InputManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+public class InputManager : MonoBehaviour {
+	/// <summary> CharacterController, которым будем управлять </summary>
+	[SerializeField]
+	private CharacterController charController = default;
+	
+	/// <summary> Скорость передвижения персонажа </summary>
+	[SerializeField]
+	private float movSpeed = 5f;
+	
+	
+
+	private void Update() {
+		Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+
+		charController.Move(move * (Time.deltaTime * movSpeed));
+	}
+}
