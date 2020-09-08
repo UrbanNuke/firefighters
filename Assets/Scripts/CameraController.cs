670da312b00da72d4de1f443d286ef157d9412c1
urbanz91 <turbino4ka91@gmail.com>
2020-09-08 19:17:35 +0300

    add: strafe animation, basic inputHandler + camera

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+	/// <summary> Объект за которым следит камера </summary>
+	public Transform followObject;
+
+	/// <summary> Угол обзора камеры относительно объекта </summary>
+	public float angleOfView;
+	/// <summary> Оффсет камеры относительно объекта по оси Z </summary>
+	public float cameraOffset;
+	/// <summary> Оффсет центральной точки относительно объекта по оси Z </summary>
+	public float centerPointOffset;
+
+	private void LateUpdate() {
+		var objectPosition = followObject.position;
+		transform.position = new Vector3(objectPosition.x, objectPosition.y + angleOfView, objectPosition.z - cameraOffset);
+		var lookAt = new Vector3(objectPosition.x, objectPosition.y, objectPosition.z - centerPointOffset);
+		transform.LookAt(lookAt);
+	}
+}
---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+namespace Player {
+	public class PlayerController : MonoBehaviour {
+		/// <summary> Скорость передвижения персонажа </summary>
+		public float walkSpeed = 5f;
+		
+		// ссылки на другие компоненты gameObject
+		private Rigidbody _rb;
+		private Animator _animator;
+
+		/// <summary> Значения ввода с клавиатуры игроком </summary>
+		private Vector3 _inputMovement;
+		
+		// Закэшированные параметры анимации
+		private static readonly int HorSpeed = Animator.StringToHash("horSpeed");
+		private static readonly int VertSpeed = Animator.StringToHash("vertSpeed");
+		
+		private void Start() {
+			_rb = GetComponent<Rigidbody>();
+			_animator = GetComponent<Animator>();
+		}
+		
+		private void Update() {
+			_inputMovement = Vector3.zero;
+			_inputMovement = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+		}
+
+		private void FixedUpdate() {
+			_rb.velocity = _inputMovement * walkSpeed;
+			_animator.SetFloat(HorSpeed, _inputMovement.x);
+			_animator.SetFloat(VertSpeed, _inputMovement.z);
+		}
+	}
+}
