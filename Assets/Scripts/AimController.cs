bffe02960a04fa73cddc3a0e4f1526bb1348cf92
urbanz91 <turbino4ka91@gmail.com>
2020-09-12 15:36:22 +0300

    add: AIM sprite, player rotation depends on aim

---
+++ Assets/Scripts/AimController.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+
+public class AimController : MonoBehaviour {
+	void Update() {
+		transform.position = Input.mousePosition;
+	}
+}
--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -8,10 +8,17 @@ namespace Player {
 		// ссылки на другие компоненты gameObject
 		private Rigidbody _rb;
 		private Animator _animator;
+		
+		// камера и ее контроллер
+		private Camera _camera;
+		private CameraController _cameraController;
 
-		/// <summary> Значения ввода с клавиатуры игроком </summary>
+		/// <summary> Значения ввода движения с клавиатуры игроком </summary>
 		private Vector3 _inputMovement;
 		
+		/// <summary> Место нахождения прицела </summary>
+		private Vector3 _aimPosition;
+
 		// Закэшированные параметры анимации
 		private static readonly int HorSpeed = Animator.StringToHash("horSpeed");
 		private static readonly int VertSpeed = Animator.StringToHash("vertSpeed");
@@ -19,15 +26,29 @@ namespace Player {
 		private void Start() {
 			_rb = GetComponent<Rigidbody>();
 			_animator = GetComponent<Animator>();
+			_camera = Camera.main;
+			if (_camera != null) {
+				_cameraController = _camera.GetComponent<CameraController>();
+			}
 		}
 		
 		private void Update() {
 			_inputMovement = Vector3.zero;
-			_inputMovement = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+			_inputMovement = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")).normalized;
+
+			Vector3 mousePos = Input.mousePosition;
+			_aimPosition = _camera.ScreenToWorldPoint(new Vector3(mousePos.x, mousePos.y, _cameraController.angleOfView));
 		}
 
 		private void FixedUpdate() {
-			_rb.velocity = _inputMovement * walkSpeed;
+			Vector3 lookPos = _aimPosition - transform.position;
+			lookPos.y = 0;
+			var rotation = Quaternion.LookRotation(lookPos);
+			_rb.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime * 5f);
+			
+			Vector3 movement = transform.TransformVector(_inputMovement * walkSpeed);
+			movement.y = _rb.velocity.y;
+			_rb.velocity = movement;
 			_animator.SetFloat(HorSpeed, _inputMovement.x);
 			_animator.SetFloat(VertSpeed, _inputMovement.z);
 		}
