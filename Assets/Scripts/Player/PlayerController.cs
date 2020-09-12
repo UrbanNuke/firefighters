ab036602f86b8f7232807194e39e339fdc44403c
urbanz91 <turbino4ka91@gmail.com>
2020-09-12 17:41:55 +0300

    add: player firing

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -12,6 +12,8 @@ namespace Player {
 		// камера и ее контроллер
 		private Camera _camera;
 		private CameraController _cameraController;
+		
+		private ParticleSystem _fireHoseParticles;
 
 		/// <summary> Значения ввода движения с клавиатуры игроком </summary>
 		private Vector3 _inputMovement;
@@ -19,14 +21,19 @@ namespace Player {
 		/// <summary> Место нахождения прицела </summary>
 		private Vector3 _aimPosition;
 
+		/// <summary> Флаг состояния атаки </summary>
+		private bool _firing;
+		
 		// Закэшированные параметры анимации
 		private static readonly int HorSpeed = Animator.StringToHash("horSpeed");
 		private static readonly int VertSpeed = Animator.StringToHash("vertSpeed");
-		
+		private static readonly int Firing = Animator.StringToHash("firing");
+
 		private void Start() {
 			_rb = GetComponent<Rigidbody>();
 			_animator = GetComponent<Animator>();
 			_camera = Camera.main;
+			_fireHoseParticles = transform.Find("FireHoseParticle").GetComponent<ParticleSystem>();
 			if (_camera != null) {
 				_cameraController = _camera.GetComponent<CameraController>();
 			}
@@ -37,7 +44,11 @@ namespace Player {
 			_inputMovement = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")).normalized;
 
 			Vector3 mousePos = Input.mousePosition;
-			_aimPosition = _camera.ScreenToWorldPoint(new Vector3(mousePos.x, mousePos.y, _cameraController.angleOfView));
+			_aimPosition = _camera.ScreenToWorldPoint(
+				new Vector3(mousePos.x, mousePos.y + 100f, _cameraController.angleOfView)
+			);
+			
+			Fire();
 		}
 
 		private void FixedUpdate() {
@@ -45,12 +56,28 @@ namespace Player {
 			lookPos.y = 0;
 			var rotation = Quaternion.LookRotation(lookPos);
 			_rb.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime * 5f);
-			
-			Vector3 movement = transform.TransformVector(_inputMovement * walkSpeed);
-			movement.y = _rb.velocity.y;
-			_rb.velocity = movement;
-			_animator.SetFloat(HorSpeed, _inputMovement.x);
-			_animator.SetFloat(VertSpeed, _inputMovement.z);
+
+			if (!_firing) {
+				Vector3 movement = transform.TransformVector(_inputMovement * walkSpeed);
+				movement.y = _rb.velocity.y;
+				_rb.velocity = movement;
+				_animator.SetFloat(HorSpeed, _inputMovement.x);
+				_animator.SetFloat(VertSpeed, _inputMovement.z);
+			}
+		}
+
+		/// <summary> Атака игрока </summary>
+		private void Fire() {
+			if (Input.GetMouseButtonDown(0)) {
+				_animator.SetBool(Firing, true);
+				_firing = true;
+				_fireHoseParticles.Play();
+			}
+			if (Input.GetMouseButtonUp(0)) {
+				_animator.SetBool(Firing, false);
+				_firing = false;
+				_fireHoseParticles.Stop();
+			} 
 		}
 	}
 }
